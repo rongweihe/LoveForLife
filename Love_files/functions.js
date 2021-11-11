a044a88d157ef5eb11b4e31007f583c14c8608ca
rongweihe <herongwei@sogou-inc.com>
2021-11-11 08:44:11 +0800

    add juanbb

--- Love_files/functions.js
+++ Love_files/functions.js
@@ -39,7 +39,7 @@ $(window).resize(function() {
 
 function timeElapse(date){
 	var current = new Date();
-	current.setFullYear(2021,10,10);
+	current.setFullYear(2021,5,11);
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
